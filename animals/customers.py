f593cbca9502bbac2edba2f3e1da206c36c13ccc
EugeneTerry <eugene.terry73@gmail.com>
2021-10-16 12:15:37 -0500

    chapter 4 done

---
+++ animals/customers.py
@@ -0,0 +1,45 @@
+CUSTOMERS = [
+  {
+    "id": 1,
+    "name": "Hannah Hall",
+    "address": "7002 Chestnut Ct",
+    "email": "HannahHall@xmail.com"
+  },
+  {
+    "id": 2,
+    "name": "Taylor Slow",
+    "address": "77A Almond Cv",
+    "email": "TaylorSlow@kmail.com"
+  },
+  {
+    "id": 3,
+    "name": "Bruno Pluto",
+    "address": "4 Cashew Dr",
+    "email": "BPluto@mars.com"
+  },
+  {
+    "id": 4,
+    "name": "Tom Jump",
+    "address": "2322 Walnut St",
+    "email": "JumpJump@me.net"
+  }
+]
+
+def get_all_customers():
+    return CUSTOMERS
+
+
+def get_single_customer(id):
+  requested_customer = None
+  for customer in CUSTOMERS:
+    if customer["id"] == id:
+      requested_customer = customer
+
+  return requested_customer
+
+def create_customer(customer):
+  max_id = CUSTOMERS[-1]["id"]
+  new_id = max_id + 1
+  customer["id"] = new_id
+  CUSTOMERS.append(customer)
+  return customer
--- request_handler.py
+++ request_handler.py
@@ -1,112 +1,136 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_all_quotes, get_random_quotes, get_all_employees, get_all_locations, get_single_animal,get_single_location, get_single_employee
+from animals import get_all_animals, get_all_employees, get_all_locations, get_single_animal,get_single_location, get_single_employee, get_all_customers, get_single_customer, create_animal, create_customer, create_employee
 
 
-# Here's a class. It inherits from another class.
-# For now, think of a class as a container for functions that
-# work together for a common purpose. In this case, that
-# common purpose is to respond to HTTP requests from a client.
 class HandleRequests(BaseHTTPRequestHandler):
+  # Here's a class. It inherits from another class.
+  # For now, think of a class as a container for functions that
+  # work together for a common purpose. In this case, that
+  # common purpose is to respond to HTTP requests from a client.
 
-    def parse_url(self, path): # enables me to use the "/animals/1" in the address bar
-        path_params = path.split("/")
-        resource = path_params[1]
-        id = None
+  def parse_url(self, path): # enables me to use the "/animals/1" in the address bar
+      path_params = path.split("/")
+      resource = path_params[1]
+      id = None
 
-        # Try to get the item at index 2
-        try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
+      # Try to get the item at index 2
+      try:
+          # Convert the string "1" to the integer 1
+          # This is the new parseInt()
+          id = int(path_params[2])
+      except IndexError:
+          pass  # No route parameter exists: /animals
+      except ValueError:
+          pass  # Request had trailing slash: /animals/
 
-        return (resource, id)  # This is a tuple
-
-
-    # Here's a class function
-    def _set_headers(self, status):
-        self.send_response(status)
-        self.send_header('Content-type', 'application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-
-    # Another method! This supports requests with the OPTIONS verb.
-    def do_OPTIONS(self):
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
-        self.end_headers()
-
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any GET request.
-    def do_GET(self):
-        self._set_headers(200)
-        response = {}  # Default response
-
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
-
-        if resource == "animals":
-            if id is not None:
-                response = f"{get_single_animal(id)}"
-
-            else:
-                response = f"{get_all_animals()}"
-
-        if resource == "locations":
-            if id is not None:
-                response = f"{get_single_location(id)}"
-
-            else:
-                response = f"{get_all_locations()}"
-        
-        if resource == "employees":
-            if id is not None:
-                response = f"{get_single_employee(id)}"
-
-            else:
-                response = f"{get_all_employees()}"
-
-        # if self.path == "/animals":
-        #     response = get_all_animals()
-        # elif self.path == "/animals":
-        #     response = get_single_animal()
-        # elif self.path == "/quotes":
-        #     response = get_all_quotes()
-        # elif self.path == "/quotes/random":
-        #     response = get_random_quotes()
-        # elif self.path == "/employees":
-        #     response = get_all_employees()
-        # elif self.path == "/locations":
-        #     response = get_all_locations()
-        # else:
-        #   response = []
-
-        
-
-        # This weird code sends a response back to the client
-        self.wfile.write(f"{response}".encode())
-
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any POST request.
-    def do_POST(self):
-        # Set response code to 'Created'
-        self._set_headers(201)
-
-        content_len = int(self.headers.get('content-length', 0))
-        post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+      return (resource, id)  # This is a tuple
 
 
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any PUT request.
-    def do_PUT(self):
-        self.do_POST()
+  # Here's a class function
+  def _set_headers(self, status):
+      self.send_response(status)
+      self.send_header('Content-type', 'application/json')
+      self.send_header('Access-Control-Allow-Origin', '*')
+      self.end_headers()
+
+  # Another method! This supports requests with the OPTIONS verb.
+  def do_OPTIONS(self):
+      self.send_response(200)
+      self.send_header('Access-Control-Allow-Origin', '*')
+      self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+      self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+      self.end_headers()
+
+  # Here's a method on the class that overrides the parent's method.
+  # It handles any GET request.
+  def do_GET(self):
+      self._set_headers(200)
+      response = {}  # Default response
+
+      # Parse the URL and capture the tuple that is returned
+      (resource, id) = self.parse_url(self.path)
+
+      if resource == "animals":
+          if id is not None:
+              response = f"{get_single_animal(id)}"
+
+          else:
+              response = f"{get_all_animals()}"
+
+      if resource == "locations":
+          if id is not None:
+              response = f"{get_single_location(id)}"
+
+          else:
+              response = f"{get_all_locations()}"
+      
+      if resource == "employees":
+          if id is not None:
+              response = f"{get_single_employee(id)}"
+
+          else:
+              response = f"{get_all_employees()}"
+
+      if resource == "customers":
+          if id is not None:
+              response = f"{get_single_customer(id)}"
+          
+          else:
+              response = f"{get_all_customers()}"
+
+
+      # if self.path == "/animals":
+      #     response = get_all_animals()
+      # elif self.path == "/animals":
+      #     response = get_single_animal()
+      # else:
+      #   response = []
+
+      
+
+      # This weird code sends a response back to the client
+      self.wfile.write(f"{response}".encode())
+
+  # Here's a method on the class that overrides the parent's method.
+  # It handles any POST request.
+  def do_POST(self):
+      self._set_headers(201)
+      content_len = int(self.headers.get('content-length', 0))
+      post_body = self.rfile.read(content_len)
+
+      # Convert JSON string to a Python dictionary
+      post_body = json.loads(post_body)
+
+      # Parse the URL
+      (resource, id) = self.parse_url(self.path)
+
+      # Initialize new animal
+      new_animal = None
+      new_customer = None
+      new_employee = None
+
+      # Add a new animal to the list. Don't worry about
+      # the orange squiggle, you'll define the create_animal
+      # function next.
+      if resource == "animals":
+          new_animal = create_animal(post_body)
+      if resource == "customers":
+          new_customer = create_customer(post_body)
+      if resource == "employees":
+          new_employee = create_employee(post_body)
+
+      # Encode the new animal and send in response
+      self.wfile.write(f"{new_animal}".encode())
+      self.wfile.write(f"{new_customer}".encode())
+      self.wfile.write(f"{new_employee}".encode())
+
+
+
+  # Here's a method on the class that overrides the parent's method.
+  # It handles any PUT request.
+  def do_PUT(self):
+      self.do_POST()
 
 
 # This function is not inside the class. It is the starting
