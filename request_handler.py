12365c38bcaab9097fdeb5632440dcfe00a7ad55
EugeneTerry <eugene.terry73@gmail.com>
2021-11-08 22:40:28 -0600

    did chapter 14

--- request_handler.py
+++ request_handler.py
@@ -136,6 +136,11 @@ class HandleRequests(BaseHTTPRequestHandler):
                 response = f"{get_single_customer(id)}"
             else:
                 response = f"{get_all_customers()}"
+        elif resource == "employees":
+          if id is not None:
+            response = f"{get_single_employee(id)}"
+          else:
+            response = f"{get_all_employees()}"
 
     # Response from parse_url() is a tuple with 3
     # items in it, which means the request was for
@@ -161,12 +166,6 @@ class HandleRequests(BaseHTTPRequestHandler):
       else:
         response = f"{get_all_locations()}"
 
-    if resource == "employees":
-      if id is not None:
-        response = f"{get_single_employee(id)}"
-
-      else:
-        response = f"{get_all_employees()}"
 
 
      
